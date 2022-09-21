ceadcfa35ec4b6d96e202376154f7cb0e5046db1
FRANKSADWAR <billyfranks98@gmail.com>
2022-09-21 11:12:58 +0300

    First React App

---
+++ First_React_App/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class ProductList extends React.Component {
+    render(){
+        return {}
+    }
+}
+
+class Product extends React.Component{
+    render(){
+        return {}
+    }
+}
+
+ReactDOM.render(<Product/>, document.getElementById('root'));
