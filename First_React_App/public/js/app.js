94349ba4f6cf84089b8d1a250a95688cb8c23120
FRANKSADWAR <billyfranks98@gmail.com>
2022-09-21 16:00:23 +0300

    immutability in state change

--- First_React_App/public/js/app.js
+++ First_React_App/public/js/app.js
@@ -2,16 +2,46 @@
  * To make the application data-driven, the Props and state change are used
  */
 
+
 class ProductList extends React.Component {
     // define state inside the constructor method AND USE IT TO HOLD DATA INSTEAD OF READING DIRECTLY FROM seed
     constructor(props){
         super(props);
         this.state = {
             products : [],
-        }
+        };
+
+        // bind the custom method to this
+        this.handleProductUpVote = this.handleProductUpVote.bind(this);
     }
+    // using the component did mount method to set the state which then displays the products
+    componentDidMount(){
+        this.setState({ 
+            products: Seed.products
+        });
+    }
+    // we are then using immutability so that the initial state of the products is not changed
     handleProductUpVote(productId){
-        console.log(productId + 'was upvoted');
+        // the map function is used to traverse the products array and returns a new array
+        const nextProducts = this.state.products.map((product) => {
+            // then a check of whether the current product matches the productId
+            if(product.id === productId){
+                // if it does, then we create a new object, copying the properties from the original product object
+                return Object.assign({}, product,{
+                    // then overwrite the vote property on the new product object to increment the vote count
+                    votes: product.votes + 1,
+                });
+            }
+            else{
+                // if the product is not the one specified by the productId, then we return the product unmodified as it is
+                return product;
+            }
+        });
+        // then the setState method is used to update the state
+        this.setState({
+            products: nextProducts,
+        });
+        //console.log(productId + 'was upvoted');
     }
     
     
