7bc4e7a03facc542b9cc48d8b9f14409f76d4ed4
FRANKSADWAR <billyfranks98@gmail.com>
2022-09-21 12:30:14 +0300

    changed to use props

--- First_React_App/public/js/app.js
+++ First_React_App/public/js/app.js
@@ -1,8 +1,20 @@
+/**
+ * To make the application data-driven, the Props and state change are used
+ */
 class ProductList extends React.Component {
     render(){
+        const product = Seed.products[0];
         return (
             <div className="ui unstackable items">
-                <Product/>
+                <Product
+                    id={product.id}
+                    title = {product.title}
+                    description ={product.description}
+                    url = {product.url}
+                    votes = {product.votes}
+                    submitterAvatarUrl = {product.submitterAvatarUrl}
+                    productImageUrl ={product.productImageUrl}
+                />
             </div>
         )
     }
