e93383988d6d45fd95d32cadbc248209f6883f1f
FRANKSADWAR <billyfranks98@gmail.com>
2022-09-24 18:57:28 +0300

    props

---
+++ Components/Props.js
@@ -0,0 +1,8 @@
+/**
+ *      Props are used to pass data to components, i.e from parent components to child components.
+ *      The parent components usually own the props that they pass to the child components.
+ * 
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
