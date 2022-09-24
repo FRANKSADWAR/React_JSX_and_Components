c4f8f3bc692ec5b4d936515800a75beb85cc6fa0
FRANKSADWAR <billyfranks98@gmail.com>
2022-09-24 19:27:25 +0300

    props

--- Components/Props.js
+++ Components/Props.js
@@ -3,6 +3,27 @@
  *      The parent components usually own the props that they pass to the child components.
  * 
  */
-
 import React from "react";
 import ReactDOM from "react-dom";
+
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Hello, Welcome to the NEWZ </h1>
+                {/**The prop is created by the parent component and then passed down to the child component */}
+                <Greeting name="adwar"/> 
+                <article>
+                    <p>Major new all over the country</p>
+                </article>
+            </div>
+        )
+    }
+}
+
+class Greeting extends React.Component {
+    render(){
+        return <h1>Hi there, {this.props.name} </h1>
+    }
+}
+ReactDOM.render(<App/>, document.getElementById('app'));
