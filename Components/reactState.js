024b93a521346676c308c7be06141aed76c239e4
FRANKSADWAR <billyfranks98@gmail.com>
2022-09-26 06:14:20 +0300

    state

---
+++ Components/reactState.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Example extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            mood: 'great',
+            hungry: false
+        };
+    }
+    render(){
+
+    }
+}
