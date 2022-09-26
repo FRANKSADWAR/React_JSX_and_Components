9d0418306c4d4cf948d013a98f74c71dc0f3fd35
FRANKSADWAR <billyfranks98@gmail.com>
2022-09-26 06:16:07 +0300

    state

--- Components/reactState.js
+++ Components/reactState.js
@@ -1,3 +1,8 @@
+/**
+ * The state of react components mpostly live within the component class itself, as compared to props, which may be from outside i.e passed from parent components.
+ * The state of the react components is initialized within the constructor and is changed using the setState function.
+ */
+
 import React from "react";
 import ReactDOM from "react-dom";
 
