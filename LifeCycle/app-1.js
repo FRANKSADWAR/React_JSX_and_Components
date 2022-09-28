6d5a8bb0e63bedf5a53fe4ddc4a7eeb82d93604d
FRANKSADWAR <billyfranks98@gmail.com>
2022-09-28 23:29:32 +0300

    create dashboard:

---
+++ LifeCycle/app-1.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class TimersDashboard extends React.Component {
+    render(){
+        return (
+            <div className="ui three column centered grid">
+                
+            </div>
+        )
+    }
+}
