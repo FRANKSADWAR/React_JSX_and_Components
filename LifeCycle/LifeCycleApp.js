af04ac9da6b44f7e7fe89aa71965a711834076a7
FRANKSADWAR <billyfranks98@gmail.com>
2022-10-02 22:11:05 +0300

    renames

--- LifeCycle/LifeCycleApp.js
+++ LifeCycle/LifeCycleApp.js
@@ -1,5 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {profile} from './profile';
+import {Profile} from './Profile';
 import { Directory } from "./Directory";
 
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            currentUsername: null
+        }
+
+        this.handleChoose = this.handleChoose.bind(this);
+        this.handleReturnToDirectoryClick = this.handleReturnToDirectoryClick.bind(this);
+    }
+
+    handleChoose(newUsername){
+        this.setState({
+            currentUsername: newUsername
+        });
+    }
+
+    handleReturnToDirectoryClick(){
+        this.setState({ currentUsername : null });
+    }
+    render(){
+        let body;
+        if(this.state.currentUsername){
+            body = <profile
+        }
+    }
+
+
+    
+}
