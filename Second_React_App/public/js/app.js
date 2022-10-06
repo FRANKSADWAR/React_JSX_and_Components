05c53e02f7321614e069624296c17d2a65a2b29f
FRANKSADWAR <billyfranks98@gmail.com>
2022-10-06 14:39:58 +0300

    pulls

--- Second_React_App/public/js/app.js
+++ Second_React_App/public/js/app.js
@@ -1,1 +1,13 @@
 
+import React from "react";
+import ReactDOM from "react-dom";
+
+class TimersDashboard extends React.Component {
+    render(){
+        return(
+            <div className="ui three column centered grid">
+
+            </div>
+        )
+    }
+}
