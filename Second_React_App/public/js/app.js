147ded4dafe2890fd69d8a469f17a68a97dbac45
FRANKSADWAR <billyfranks98@gmail.com>
2022-10-06 17:21:25 +0300

    skeleton

--- Second_React_App/public/js/app.js
+++ Second_React_App/public/js/app.js
@@ -1,8 +1,3 @@
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-
 /**
  * In this Timer List application, the components are first broken down from top to bottom, from the parent component to the children components.
  *  The parent component contains the EditableTimerList and the ToggableTimerForm components. 
@@ -105,13 +100,14 @@ class TimerForm extends React.Component {
 
 /**
  * This is a wrapper around the TimerForm, it will display "+" or a TimerForm 
+ * The component will not pass any props to the TimerForm component, hence the fields title and project will remain/rendered empty
  */
 class ToggleableTimerForm extends React.Component {
     render(){
         if(this.props.isOpen){
             return (
                 <TimerForm/>
-            )
+            );
         }
         else{
             return (
@@ -120,7 +116,43 @@ class ToggleableTimerForm extends React.Component {
                         <i className="plus icon"/>
                     </button>
                 </div>
-            )
+            );
         }
     }
 }
+
+class Timer extends React.Component {
+    render(){
+        const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+        return (
+            <div className="ui centered card">
+                <div className="content">
+                    <div className="header">
+                        {this.props.title}
+                    </div>
+                    <div className="meta">
+                        {this.props.project}
+                    </div>
+                    <div className="center aligned description">
+                        <h2>
+                            {elapsedString}
+                        </h2>
+                    </div>
+                    <div className="extra content">
+                        <span className="right floated edit icon">
+                            <i className="edit icon"/>
+                        </span>
+                        <span className="right floated trash icon">
+                            <i className="trash icon"/>
+                        </span>
+                    </div>
+                    <div className="ui bottom attached blue basic button">
+                        Start
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<TimersDashboard/>, document.getElementBuId('app'));
