066e8ef8d134bdba4b916c7b9eced5b9c13cc9d3
FRANKSADWAR <billyfranks98@gmail.com>
2022-11-04 15:02:40 +0300

    passed

--- Components/ParentsAndChildren_State/State_to_stateless.js
+++ Components/ParentsAndChildren_State/State_to_stateless.js
@@ -7,7 +7,7 @@ class Parent extends React.Component {
         this.state = {
             name : 'Darth Vader'
         }
-
+        this.changeName = this.changeName.bind(this);
     }
 
     changeName(newName){
@@ -16,16 +16,10 @@ class Parent extends React.Component {
         });
     }
 
-    changeNameTest(e){
-        let newName = e.target.value;
-        this.setState({
-            name: newName
-        });
-    }
 
     render(){
         return (
-            <ChildClass/>
+            <ChildClass onChange={this.changeName}/>
         );
     }
 
@@ -34,6 +28,16 @@ class Parent extends React.Component {
 }
 
 class ChildClass extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e){
+        const name = e.target.value;
+        this.props.onChange(name);
+    }
+
     render(){
         return (
             <div>
