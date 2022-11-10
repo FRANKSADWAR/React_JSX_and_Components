c564b701528debf3b4a30b39f7eb23806dadad29
FRANKSADWAR <billyfranks98@gmail.com>
2022-11-10 17:52:12 +0300

    styles

---
+++ Advanced_Concepts_React/styles.js
@@ -0,0 +1,13 @@
+const stylesTypes = {
+    background: '',
+    fontWeight: 'normal',
+    fontSize: '2em',
+    fontSizeHeader: '3em',
+    fontSizeTable: '1.5em',
+    activeLink: '#ffcc00',
+    disabledLink: '#ffffff',
+    selectedLink: 'border 1px 1px solid',
+    paddingHeader: '0.2em',
+    
+
+};
