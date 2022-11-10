68f2f4a93f93837e167f7e44a16d6d9960593281
FRANKSADWAR <billyfranks98@gmail.com>
2022-11-10 21:11:15 +0300

    misbehavour

---
+++ Advanced_Concepts_React/SliderComponents.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const GUINEAPATHS = [
+    'https://content.codecademy.com/courses/React/react_photo-guineapig-1.jpg',
+    'https://content.codecademy.com/courses/React/react_photo-guineapig-2.jpg',
+    'https://content.codecademy.com/courses/React/react_photo-guineapig-3.jpg',
+    'https://content.codecademy.com/courses/React/react_photo-guineapig-4.jpg'
+];
+
+
+class GuineaPigsContainer extends React.Component {
+}
+
