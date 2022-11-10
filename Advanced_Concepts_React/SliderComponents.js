4e96eaa4c90790e3f47ba23af366ed569380c1a5
FRANKSADWAR <billyfranks98@gmail.com>
2022-11-10 21:28:11 +0300

    created

--- Advanced_Concepts_React/SliderComponents.js
+++ Advanced_Concepts_React/SliderComponents.js
@@ -10,5 +10,43 @@ const GUINEAPATHS = [
 
 
 class GuineaPigsContainer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            currentGP : 0
+        }
+        this.interval = null;
+        this.nextGP = this.nextGP.bind(this);
+    }
+
+    nextGP(){
+        let current = this.state.currentGP;
+        let next = ++current % GUINEAPATHS.length; // use the pre-increment value with a floor/modulus operation on the number of guinea pigs in the array
+        this.setState({ currentGP : next });
+    }
+
+    componentDidMount(){
+        this.interval = setInterval(this.nextGP, 5000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.interval);
+    }
+
+    render(){
+        const src = GUINEAPATHS[this.state.currentGP];
+        return <GuineaPigs src={src}/>;
+    }
 }
 
+class GuineaPigs extends React.Component {
+    render(){
+        let src = this.props.src;
+        return (
+            <div>
+                
+            </div>
+        )
+
+    }
+}
