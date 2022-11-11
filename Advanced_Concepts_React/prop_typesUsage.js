dfa2ce027a9d3264e168061bd61c4267d03514ed
FRANKSADWAR <billyfranks98@gmail.com>
2022-11-11 10:52:38 +0300

    Created

--- Advanced_Concepts_React/prop_typesUsage.js
+++ Advanced_Concepts_React/prop_typesUsage.js
@@ -2,5 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 /**
- * propTypes are useful 
+ * propTypes are useful for prop validation and documentation.
+ * The value of a propType is an object, not a function
+ * 
  */
