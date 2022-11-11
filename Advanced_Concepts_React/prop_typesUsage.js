be6e04edc7c2dcaa416233e6817b05f347a52890
FRANKSADWAR <billyfranks98@gmail.com>
2022-11-11 10:54:38 +0300

    propTypes

--- Advanced_Concepts_React/prop_typesUsage.js
+++ Advanced_Concepts_React/prop_typesUsage.js
@@ -6,3 +6,8 @@ import PropTypes from "prop-types";
  * The value of a propType is an object, not a function and is decleared after the component has been defined
  * 
  */
+class MessageDisplayer extends React.Component {
+    render(){
+        return <h1>{this.props.message}</h1>;
+    }
+}
