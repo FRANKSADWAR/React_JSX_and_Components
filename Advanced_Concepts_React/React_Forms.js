a0e969b32acdc19cb777f65a04b3dbb3e1e9b291
FRANKSADWAR <billyfranks98@gmail.com>
2022-11-11 13:13:52 +0300

    forms

--- Advanced_Concepts_React/React_Forms.js
+++ Advanced_Concepts_React/React_Forms.js
@@ -7,6 +7,7 @@ class Input extends React.Component {
         this.state = {
             userInput: ''
         };
+        
         this.handleUserInput = this.handleUserInput.bind(this);
     }
 
