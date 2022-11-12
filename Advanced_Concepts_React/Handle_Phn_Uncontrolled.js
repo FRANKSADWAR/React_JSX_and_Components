f36ae38cf69aafa4db4fb5b82409b54eb51a97ca
FRANKSADWAR <billyfranks98@gmail.com>
2022-11-12 10:55:24 +0300

    uncontrolled

--- Advanced_Concepts_React/Handle_Phn_Uncontrolled.js
+++ Advanced_Concepts_React/Handle_Phn_Uncontrolled.js
@@ -1,3 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function PhoneNumberForm(){
+    const numberRef = React.createRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const number = numberRef.current.value;
+        
+        if( (!Number.isNaN(Number(number))) ){
+            alert('Error. Only numbers are allowed.');
+        }
+        else if(number.length <=10){
+            alert('Error. Number length exceeds 10 digits');
+        }
+        else{
+            alert(`Sending confirmation code to ${number}.`);
+        }
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Phone Number: 
+                <input type="tel" ref={numberRef}/>
+            </label>
+            <input type="submit" value="Submit"/>
+        </form>
+    )
+}
