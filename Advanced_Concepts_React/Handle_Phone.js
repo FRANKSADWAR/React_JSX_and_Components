54590b4214d808934aab986d4b68549c0cd67f45
FRANKSADWAR <billyfranks98@gmail.com>
2022-11-12 10:46:39 +0300

    created

--- Advanced_Concepts_React/Handle_Phone.js
+++ Advanced_Concepts_React/Handle_Phone.js
@@ -29,3 +29,12 @@ function PhoneNumberForm(){
 const app = document.getElementById("app");
 ReactDOM.render(<PhoneNumberForm/>,app);
 
+/**
+ * In this example, the PhoneNumber component controls the <input> element by assigning its own number state value to the value
+ * attribute. Doing this turns the off the form's default behaviour of controlling its own state.
+ * To keep the number state value up to date, an onChange handler is registered which can set the state value each time a change
+ * is detected.
+ * With this approach, the form can be updates on every change, for example making sure that the form only accepts numbers abd
+ * the numbers do not exceed 10 digits.
+ * 
+ */
