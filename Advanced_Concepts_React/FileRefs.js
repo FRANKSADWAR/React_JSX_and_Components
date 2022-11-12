8c18957e61c93d4cb4474f5ae1e7074004c275e8
FRANKSADWAR <billyfranks98@gmail.com>
2022-11-12 11:14:28 +0300

    files

--- Advanced_Concepts_React/FileRefs.js
+++ Advanced_Concepts_React/FileRefs.js
@@ -24,3 +24,8 @@ function FileForm_Uncontrrolled(){
 
 const app = document.getElementById("file_app");
 ReactDOM.render(<FileForm_Uncontrrolled/>,app);
+
+/**
+ *  The uploaded file is stored in the array like FileList returned by fileRef.current.files and the size property
+ * of this file is accessed when the user submits the form
+ */
